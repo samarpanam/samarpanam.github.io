aed803f7cfa3e9088d2f6924a8f8a6b420a7799e
Arnab Bhattacharya <arnabb@cse.iitk.ac.in>
2020-09-09 23:36:25 +0530

    done?

--- data.py
+++ data.py
@@ -16,7 +16,7 @@ site = {
         'events': 'Events',
         'about': 'About',
         'members': 'Members',
-        'contact': 'Contact'
+        #'contact': 'Contact'
     }
 }
 
@@ -31,8 +31,7 @@ pages['index'] = {
 pages['events'] = {
     'title': 'Events',
     'events': [
-        {'name': 'Conclave', 'venue': 'Internet', 'date': '27th September, 2020 (Saturday)', 'time': '9:30am-12:30pm'},
-        #{'name': 'Event 2', 'venue': 'TBD', 'time': '8pm'}
+        {'name': 'Conclave', 'venue': 'Webex', 'date': '27th September, 2020 (Sunday)', 'time': '5:00pm-8:00pm'},
     ],
     'show_events': True
 }
@@ -46,14 +45,23 @@ pages['about'] = {
 pages['members'] = {
     'title': 'Members',
     'members': [
-        {'name': 'Anil Gourishetty', 'email': 'abc@abc.com', 'institution': 'IIT Roorkee'},
+        {'name': 'Anil Kumar Gourishetty', 'email': 'abc@abc.com', 'institution': 'IIT Roorkee'},
         {'name': 'Anuradha Choudry', 'email': 'abc@abc.com', 'institution': 'IIT Kharagpur'},
         {'name': 'Anway Mukhopadhyay', 'email': 'abc@abc.com', 'institution': 'IIT Kharagpur'},
+        {'name': 'Arnab Bhattacharya', 'email': 'abc@abc.com', 'institution': 'IIT Kanpur'},
+        {'name': 'G S Murthy', 'email': 'abc@abc.com', 'institution': 'IIT Indore'},
+        {'name': 'Ganesh Ramakrishnan', 'email': 'abc@abc.com', 'institution': 'IIT Bombay'},
+        {'name': 'Nomesh Bolla', 'email': 'abc@abc.com', 'institution': 'IIT Delhi'},
+        {'name': 'Prathosh', 'email': 'abc@abc.com', 'institution': 'IIT Delhi'},
+        {'name': 'Ravi Kiran', 'email': 'abc@abc.com', 'institution': 'IIIT Hyderabad'},
+        {'name': 'Venkateswara Pai', 'email': 'abc@abc.com', 'institution': 'IISER Pune'},
     ],
     'subtitle': 'Advisory Board',
     'mentors': [
-        {'name': 'Ramasubramanian', 'email': 'abc@abc.com', 'institution': 'IIT Bombay'},
-        {'name': 'Shrinivasa Varakhedi', 'email': 'abc@abc.com', 'institution': 'Kaviguru'},
+        {'name': 'K Ramasubramanian', 'email': 'abc@abc.com', 'institution': 'IIT Bombay'},
+        {'name': 'M D Srinivas', 'email': 'abc@abc.com', 'institution': 'Centre for Policy Studies, Chennai'},
+        {'name': 'Srinivasa Varakhedi', 'email': 'abc@abc.com', 'institution': 'Kavikulaguru Kalidas Sanskrit University'},
+        {'name': 'Subhash Kak', 'email': 'abc@abc.com', 'institution': 'Oklahoma State University, USA'},
     ]
 }
 
